299107f1cef4dc444c75268fbd7b83400e88f58d
WONTAE\spald <wontae.gramin9@gmail.com>
2025-01-02 10:29:37 +0000

    index.html에서 main.jsx → main.tsx로 import 수정해주기

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./ui/ErrorFallback";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ErrorBoundary
       FallbackComponent={ErrorFallback}
