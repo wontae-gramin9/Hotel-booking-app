6de47312d8b8e4f7115653f8539dde1bde0d7733
WONTAE\spald <wontae.gramin9@gmail.com>
2025-01-26 14:43:26 +0000

    [Add] Input 받아와서 ping 하기

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,3 +1,38 @@
+import os
+import requests
+
+def ping_url(website_url, delay, max_trials):
+  trials = 0
+
+  while trials < max_trials:
+    try:
+      response = requests.get(website_url)
+      if response.status_code == 200:
+        print(f"Website {website_url} is reachable.")
+        return True
+    except requests.ConnectionError:
+      print(f"Website {website_url} is unreachable. Retrying in {delay} seconds...")
+      time.sleep(delay)
+      trials += 1
+    except requests.exceptions.MissingSchema:
+      print(f"Invalid URL format: {website_url}. Make sure the URL has a valid schema")
+      return False
+  
+    return False
+
+
+def run():
+  # Fetch the inputs
+  website_url = os.getenv('INPUT_URL')
+  delay = int(os.getenv('INPUT_DELAY'))
+  max_trials = int(os.getenv('INPUT_MAX_TRIALS'))
+  website_reachable = ping_url(website_url, delay, max_trials)
+
+  if not website_reachable:
+    raise Exception(f"Website: {website_url} is malformed or unreachable.")
+
+  print(f"Website {website_url} is reachable.")
+
 if __name__ == "__main__":
-  print("Hello word")
+  run()
   
