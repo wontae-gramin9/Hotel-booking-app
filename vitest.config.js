7b2b059fdead2098775c0777ed1d8d377b2a97a0
WONTAE\spald <wontae.gramin9@gmail.com>
2025-01-29 20:30:05 +0000

    [Fix] broswer API (window, localStorage)를 위한 browser test mode 설치

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,12 @@ import { resolve } from "path";
 export default defineConfig({
   test: {
     environment: "jsdom",
+    browser: {
+      provider: "playwright",
+      enabled: true,
+      headless: true,
+      instances: [{ browser: "chromium" }],
+    },
   },
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
