05d73da6aac18da24f9d2bf257d28eaa275e3f9d
WONTAE\spald <wontae.gramin9@gmail.com>
2025-01-30 10:07:41 +0000

    [Fix] react-query QueryClientProvider 누락된 것 추가

--- src/tests/test-utils.tsx
+++ src/tests/test-utils.tsx
@@ -1,13 +1,25 @@
-// https://testing-library.com/docs/react-testing-library/setup#custom-render
 import { ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { DarkModeProvider } from "@/context/DarkModeContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// 공식 DOC에서 새로운 객체 만들어서 전달하라고 나와있음
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
+// https://testing-library.com/docs/react-testing-library/setup#custom-render
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <DarkModeProvider>
-      <BrowserRouter>{children}</BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </QueryClientProvider>
     </DarkModeProvider>
   );
 };
