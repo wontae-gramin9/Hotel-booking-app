d18a2c57a78eb6e255618a14265adbe6add06f3d
WONTAE\spald <wontae.gramin9@gmail.com>
2025-01-30 12:06:33 +0000

    [Add] react query customRenderHook

--- src/tests/test-utils.tsx
+++ src/tests/test-utils.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react";
-import { render, RenderOptions } from "@testing-library/react";
+import { render, renderHook, RenderOptions } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import { DarkModeProvider } from "@/context/DarkModeContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -35,10 +35,13 @@ const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
+const customRenderHook = (customHook: Function) =>
+  renderHook(() => customHook(), { wrapper: QueryClientWrapper });
+
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, "wrapper">
 ) => render(ui, { wrapper: AllTheProviders, ...options });
 
 export * from "@testing-library/react";
-export { customRender as render };
+export { customRender as render, customRenderHook as renderHook };
